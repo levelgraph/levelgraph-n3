65db3cb45c402bae2d857fe756de1e22b1240e31
Matteo Collina <matteo.collina@gmail.com>
2013-07-10 12:29:31 +0100

    Added support for 'a' conversion in the 'get'.

--- test/get_spec.js
+++ test/get_spec.js
@@ -15,16 +15,54 @@ describe("n3.get", function() {
     db.close(done);
   });
 
-  it("should convert a single triple into N3", function(done) {
+  it("should convert a single triple into n3", function(done) {
     db.put({
-        subject: "http://example.org/cartoons#Tom"
-      , predicate: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type'
-      , object: 'http://example.org/cartoons#cat'
+        subject: "http://example.org/cartoons#tom"
+      , predicate: "http://example.org/cartoons#dumberthan"
+      , object: "http://example.org/cartoons#jerry"
     }, function() {
+      db.n3.get({
+        subject: "http://example.org/cartoons#tom"
+      }, function(err, triples) {
+        expect(triples).to.eql("<http://example.org/cartoons#tom> <http://example.org/cartoons#dumberthan> <http://example.org/cartoons#jerry> .\n");
+        done();
+      });
+    });
+  });
+
+  it("should convert 'rdf-type' to 'a'", function(done) {
+    db.put({
+        subject: "http://example.org/cartoons#tom"
+      , predicate: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+      , object: "http://example.org/cartoons#cat"
+    }, function() {
+      db.n3.get({
+        subject: "http://example.org/cartoons#tom"
+      }, function(err, triples) {
+        var expected = "<http://example.org/cartoons#tom> a <http://example.org/cartoons#cat> .\n";
+        expect(triples).to.eql(expected);
+        done();
+      });
+    });
+  });
+
+  it("should convert two triples into N3, reusing the subject", function(done) {
+    db.put([{
+        subject: "http://example.org/cartoons#Tom"
+      , predicate: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+      , object: "http://example.org/cartoons#cat"
+    }, {
+        subject: "http://example.org/cartoons#Tom"
+      , predicate: "http://example.org/cartoons#dumberThan"
+      , object: "http://example.org/cartoons#Jerry"
+    }], function() {
       db.n3.get({
         subject: "http://example.org/cartoons#Tom"
       }, function(err, triples) {
-        expect(triples).to.eql("<http://example.org/cartoons#Tom> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://example.org/cartoons#cat> .\n");
+        var expected = "" +
+          "<http://example.org/cartoons#Tom> <http://example.org/cartoons#dumberThan> <http://example.org/cartoons#Jerry> ;\n" + 
+          "    a <http://example.org/cartoons#cat> .\n";
+        expect(triples).to.eql(expected);
         done();
       });
     });
